f146d88ddaf8b3ea53575485a6f3512f4e50bd47
0rigin-c0de <shunnu807@gmail.com>
2023-12-18 14:53:21 +0530

    initial-commit

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./posts.css";
+import Post from "../Post/Post";
+
+function Posts(props) {
+  return (
+    <div className="posts container-fluid row">
+      {props.posts.map((post) => (
+        <div className="col-md-6" key={post._id}>
+          <Post post={post} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Posts;
